43d3844ea2ebc474e867c5dcc61473195f876ae1
labrat97 <james@nineseven.net>
2023-12-03 12:36:23 -0800

    Merged in patch from uconsole repo.

--- drivers/staging/vc04_services/bcm2835-audio/bcm2835.c
+++ drivers/staging/vc04_services/bcm2835-audio/bcm2835.c
@@ -10,9 +10,10 @@
 #include "bcm2835.h"
 #include <soc/bcm2835/raspberrypi-firmware.h>
 
-static bool enable_hdmi, enable_hdmi0, enable_hdmi1;
-static bool enable_headphones = true;
 static int num_channels = MAX_SUBSTREAMS;
+static bool enable_hdmi = true, enable_hdmi0, enable_hdmi1;
+static bool enable_headphones = true;
+static bool enable_compat_alsa;
 
 module_param(enable_hdmi, bool, 0444);
 MODULE_PARM_DESC(enable_hdmi, "Enables HDMI virtual audio device");
